b3d6dd7cc1af57bc23a84c01e26b9c408f774e05
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2018-11-13 11:22:46 +0900

    add value

--- force-app/main/default/aura/EditRow/EditRowController.js
+++ force-app/main/default/aura/EditRow/EditRowController.js
@@ -1,5 +1,23 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(c, e, h) {
+        var rec = c.get('v.Record');
 
+        var f1 = c.get('v.Field1');
+        if (f1 !== 'undefined') c.set('v.Value1', rec[f1])
+
+        var f2 = c.get('v.Field2');
+        if (f2 !== 'undefined') c.set('v.Value2', rec[f2])
+
+        var f3 = c.get('v.Field3');
+        if (f3 !== 'undefined') c.set('v.Value3', rec[f3])
+
+        var f4 = c.get('v.Field4');
+        if (f4 !== 'undefined') c.set('v.Value4', rec[f4])
+
+        var f5 = c.get('v.Field5');
+        if (f5 !== 'undefined') c.set('v.Value5', rec[f5])
+
+        var f6 = c.get('v.Field6');
+        if (f6 !== 'undefined') c.set('v.Value6', rec[f6])
     }
 })
