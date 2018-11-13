03764b5705a6b3bf4ca168077540c823c7a218f8
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2018-11-13 13:36:22 +0900

    shaped table

--- force-app/main/default/aura/ChildRecordsBatchEditor/ChildRecordsBatchEditorController.js
+++ force-app/main/default/aura/ChildRecordsBatchEditor/ChildRecordsBatchEditorController.js
@@ -1,5 +1,30 @@
 ({
     init : function(c, e, h) {
-        h.initHelper(c);
+        var columns = [];
+
+        var f1 = c.get('v.Field1');
+        if (f1) columns.push(f1)
+
+        var f2 = c.get('v.Field2');
+        if (f2) columns.push(f2)
+
+        var f3 = c.get('v.Field3');
+        if (f3) columns.push(f3)
+
+        var f4 = c.get('v.Field4');
+        if (f4) columns.push(f4)
+
+        var f5 = c.get('v.Field5');
+        if (f5) columns.push(f5)
+        
+        var f6 = c.get('v.Field6');
+        if (f6) columns.push(f6)
+
+        h.initHelper(c, columns);
+        h.fetchRecords(c);
+    },
+
+    onSave: function(c, e, h) {
+        console.log('save')
     }
 })
--- force-app/main/default/aura/ChildRecordsBatchEditor/ChildRecordsBatchEditorHelper.js
+++ force-app/main/default/aura/ChildRecordsBatchEditor/ChildRecordsBatchEditorHelper.js
@@ -1,14 +1,37 @@
 ({
-    initHelper : function(c) {
+    initHelper: function (c, columns) {
         var action = c.get('c.doInit')
         action.setParams({
-            'parentId': c.get('v.recordId'),
+            'objName': c.get('v.ChildName'),
+            'columns': columns,
         })
-        action.setCallback(this, function(res){
+        action.setCallback(this, function (res) {
             var status = res.getState();
-            if (status === 'SUCCESS') c.set('v.ChildRecords', res.getReturnValue())
-            else alert('error')
+            if (status === 'SUCCESS') {
+                var info = [];
+                var ret = res.getReturnValue();
+                if (ret) {
+                    ret.forEach(function (r) {
+                        info.push(JSON.parse(r));
+                    })
+                    c.set('v.Columns', info);
+                    console.log(c.get('v.Columns'))
+                }
+            } else alert('error')
         })
         $A.enqueueAction(action)
+    },
+
+    fetchRecords: function(c) {
+        var action = c.get('c.fetchRecords');
+        action.setParams({
+            'ObjName': c.get('v.ChildName'),
+            'parentId': c.get('v.recordId')
+        });
+        action.setCallback(this, function(res){
+            if (res.getState() === 'SUCCESS') c.set('v.ChildRecords', res.getReturnValue())
+            console.log(c.get('v.ChildRecords'))
+        });
+        $A.enqueueAction(action);
     }
 })
